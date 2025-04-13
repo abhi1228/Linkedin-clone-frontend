aa84372d80ede9a754ec63e56fb138de093fec09
ABHISHEK <ag199680.ag@gmail.com>
2025-04-14 01:20:41 +0530

    url configured with live hosting

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,6 +1,6 @@
 const {default:axios}=require("axios");
 
-export const BASE_URL="http://localhost:9080";
+export const BASE_URL="https://linkedin-clone-backend-19dd.onrender.com/";
 
 export const clientServer=axios.create({
     baseURL:BASE_URL
